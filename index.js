a3aad7dc1eb2d9c66a921f6050dc864ee717e0d1
merazhossain5662-hash <merazhossain5662@gmail.com>
2026-05-26 18:16:31 +0600

    trying to fix the API prodaction error

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if(!header){
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     // await client.connect();
     
     const db = client.db("DocAppoint")
